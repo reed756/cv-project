c9a2ac3e17b5291d78705435b0d7b0af154a4d7b
reed756 <68922323+reed756@users.noreply.github.com>
2021-07-30 14:48:00 +0100

    added template component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './styles/App.css';
 import General from './components/General';
 import Education from './components/Education';
 import Work from './components/Work';
+import Template from './components/Template';
 
 function App() {
     return (
       <div className="App">
         <Heading />
-        <General />
-        <Education />
-        <Work />
+        <div className="content">
+          <div className="forms">
+            <General />
+            <Education />
+            <Work />
+          </div>
+          <div className="preview">
+            <Template />
+          </div>
+        </div>
       </div>
     );
 }
---
+++ src/components/Template.js
@@ -0,0 +1,14 @@
+import { div } from 'prelude-ls';
+import React from 'react';
+
+class Template extends React.Component {
+    render() {
+        return (
+            <div className="template">
+
+            </div>
+        )
+    }
+}
+
+export default Template;
